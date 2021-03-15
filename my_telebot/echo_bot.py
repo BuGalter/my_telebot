cbfe5a518a254eda537760df8d8a222a8b63efa5
BuGalter <wardoc78@gmail.com>
2021-03-15 20:51:19 +0300

    Added to echo_bot.py simple examle from documentation

---
+++ my_telebot/echo_bot.py
@@ -0,0 +1,22 @@
+import telebot
+from dotenv import dotenv_values
+
+PATH_TO_TOKEN = '.env'
+
+def get_token(path_to_token):
+    my_env_variables = dotenv_values(path_to_token)
+    return my_env_variables['TOKEN']
+
+
+TOKEN = get_token(PATH_TO_TOKEN)
+bot = telebot.TeleBot("TOKEN")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
