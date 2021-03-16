bf27252fa62514d73aeb82ad9ddfd92380f3b85c
BuGalter <wardoc78@gmail.com>
2021-03-16 20:09:20 +0300

    Added commands and keboard to bot

--- my_telebot/echo_bot.py
+++ my_telebot/echo_bot.py
@@ -10,13 +10,25 @@ def get_token(path_to_token):
 
 token = get_token(PATH_TO_TOKEN)
 bot = telebot.TeleBot(token = token)
+keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard.row('/start', '/help', '/settings', 'i love you')
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start', 'help', 'settings'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Hello! how are you?", reply_markup=keyboard)
 
+"""
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
+"""
 
+@bot.message_handler(content_types = ['text'])
+def send_text(message):
+    if message.text.lower() == 'i love you':
+        bot.send_message(message.chat.id, 'I love You to', reply_markup=keyboard)
+    else:
+        bot.send_message(message.chat.id, 'I not understend you.', reply_markup=keyboard)
+
+print('Bot start!')
 bot.polling()
